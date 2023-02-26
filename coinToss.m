4d10a5d2646db00a0a2c90d5ea40bca60aa8dd07
Luis Fernando Monroy <97465921+LuisMonroy1@users.noreply.github.com>
2023-02-25 23:27:27 -0600

    Add files via upload

---
+++ coinToss.m
@@ -0,0 +1,9 @@
+%coinToss.m
+%a script that flips a fair coin and displays the output
+
+
+if rand<0.5 %if a random number is less than .5 say heads
+    disp('HEADS');
+else        %if greater than 0.5 say tails
+    disp('TAILS');
+end
